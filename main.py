2a87e26ba464c5a19559f424e7083f3b7c325d8c
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-12-31 16:59:10 -0600

    it should work now

--- main.py
+++ main.py
@@ -136,6 +136,22 @@ resources = {
     "coffee": 100
 }
 
+# def adding ():
+#     quarters = int(input("Please insert your quarters")) * .25 
+#     dimes = int(input("Please insert your dimes")) * .10 
+#     nickels = int(input("Please insert your nickels")) * .05 
+#     pennies = int(input("Please insert your pennies")) * .01 
+#     total_money = quarters + dimes + nickels + pennies 
+#     return total_money
+
+# money = adding
+
+
+
+# print(MENU["espresso"]["cost"])
+
+money = 0
+
 is_on = True
 
 while is_on:
@@ -156,6 +172,8 @@ while is_on:
         else:
             resources["water"] =   resources["water"] - MENU["espresso"]["ingredients"]["water"]
             resources["coffee"] =   resources["coffee"] - MENU["espresso"]["ingredients"]["coffee"]
+
+
     elif choice == "latte":
         if resources["water"] < 200:
             print("There is not enough water")
@@ -163,6 +181,34 @@ while is_on:
             print("There is not enough Coffee")
         elif resources["milk"] < 150:
             print("There is not enough Milk")
+        else:
+
+                print("Please insert coints")
+                num_quarters = int(input("How many quarters?"))
+                num_dimes = int(input("How many dimes?"))
+                num_nickels = int(input("How many nickels?"))
+                num_pennies = int(input("How many pennies?"))
+                quarters = num_quarters * .25
+                dimes = num_dimes * .10
+                nickels = num_nickels * .05
+                pennies = num_pennies * .01
+                total_paid = quarters + dimes + nickels + pennies 
+            if total_paid < MENU["latte"]["cost"]:
+                print("Your money is refunded. You dont have enough")
+            elif total_paid == MENU["latte"]["cost"]:
+                print("You have no change, you paid exact amount!")
+                resources["water"] =   resources["water"] - MENU["latte"]["ingredients"]["water"]
+                resources["coffee"] =   resources["coffee"] - MENU["latte"]["ingredients"]["coffee"]
+                resources["milk"] =   resources["milk"] - MENU["latte"]["ingredients"]["milk"]
+                money+= cost
+            elif total_paid > MENU["latte"]["cost"]:
+                change = total_paid - MENU["latte"]["cost"]
+                print(f"Your change is: {change} ")
+                resources["water"] =   resources["water"] - MENU["latte"]["ingredients"]["water"]
+                resources["coffee"] =   resources["coffee"] - MENU["latte"]["ingredients"]["coffee"]
+                resources["milk"] =   resources["milk"] - MENU["latte"]["ingredients"]["milk"]
+                money += cost
+
     elif choice == "cappuccino":
         if resources["water"] < 250:
             print("There is not enough water")
@@ -170,6 +216,15 @@ while is_on:
             print("There is not enough Coffee")
         elif resources["milk"] < 100:
             print("There is not enough Milk")
+        else:
+            resources["water"] =   resources["water"] - MENU["cappuccino"]["ingredients"]["water"]
+            resources["coffee"] =   resources["coffee"] - MENU["cappuccino"]["ingredients"]["coffee"]
+            resources["milk"] =   resources["milk"] - MENU["cappuccino"]["ingredients"]["milk"]
+
 
 
 #Resources is completed
+
+#On number 5 now. This will have an if or elif statement
+    
+
